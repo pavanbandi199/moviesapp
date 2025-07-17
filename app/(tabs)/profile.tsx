af8b2f4f683567a1ed1bb80cfba0d7995e31b91c
pavanbandi199 <pavanbandi1999@gmail.com>
2025-07-17 23:26:52 +0530

    last push

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -10,7 +10,7 @@ const Profile = () => {
      <SafeAreaView className="bg-primary flex-1 px-10">
       <View className="flex justify-center items-center flex-1 flex-col gap-5">
         <Image source={icons.person} className="size-10" tintColor="#fff" />
-        <Text className="text-gray-500 text-base">Save</Text>
+        <Text className="text-gray-500 text-base">Save Profile</Text>
       </View>
     </SafeAreaView>
   )
